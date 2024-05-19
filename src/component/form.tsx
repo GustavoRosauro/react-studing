796cc821809e4eca0d3439e4e755d4d465ed67dd
GustavoRosauro <gustavosrosauro@gmail.com>
2024-05-19 17:52:10 +0100

    code formated

--- src/component/form.tsx
+++ src/component/form.tsx
@@ -6,13 +6,13 @@ import { Button, Container, Paper, Stack, styled, Table, TableBody, TableCell, t
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
     [`&.${tableCellClasses.head}`]: {
-      backgroundColor: theme.palette.common.black,
-      color: theme.palette.common.white,
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white,
     },
     [`&.${tableCellClasses.body}`]: {
-      fontSize: 14,
+        fontSize: 14,
     },
-  }));
+}));
 
 const Form: React.FC = () => {
     const [course, setCourse] = useState<CourseModel>({ name: '' })
@@ -34,46 +34,46 @@ const Form: React.FC = () => {
     }
     return (
         <Container >
-                    <Container >
+            <Container >
 
-            <h1>My form</h1>
-            <Stack
-            direction={{ xs: 'column', sm: 'row' }}
-            alignSelf="center"
-            spacing={1}
-            useFlexGap
-            sx={{ pt: 2, width: { xs: '100%', sm: 'auto' } }}
-          >
-            <TextField variant='outlined'
-                name='name'
-                label='course'
-                onChange={handleInputEvent}
-                value={course.name}
-                size='small'></TextField>
-            <Button variant='contained' color='success' onClick={saveCourse}>Save</Button>
-            </Stack>
+                <h1>My form</h1>
+                <Stack
+                    direction={{ xs: 'column', sm: 'row' }}
+                    alignSelf="center"
+                    spacing={1}
+                    useFlexGap
+                    sx={{ pt: 2, width: { xs: '100%', sm: 'auto' } }}
+                >
+                    <TextField variant='outlined'
+                        name='name'
+                        label='course'
+                        onChange={handleInputEvent}
+                        value={course.name}
+                        size='small'></TextField>
+                    <Button variant='contained' color='success' onClick={saveCourse}>Save</Button>
+                </Stack>
             </Container>
             <br></br>
             <Container >
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} size="small">
-                    <TableHead>
-                        <TableRow>
-                            <StyledTableCell>Course</StyledTableCell>
-                        </TableRow>
-                    </TableHead>
-                            <TableBody>
-                                {courses.map(c =>
-                                    <TableRow>
-                                        <TableCell>{c.name}</TableCell >
-                                    </TableRow>
-                                )}
-                            </TableBody>
-                </Table>
-            </TableContainer>
-                                </Container>
-                                </Container>
-                );
+                <TableContainer component={Paper}>
+                    <Table sx={{ minWidth: 650 }} size="small">
+                        <TableHead>
+                            <TableRow>
+                                <StyledTableCell>Course</StyledTableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {courses.map(c =>
+                                <TableRow>
+                                    <TableCell>{c.name}</TableCell >
+                                </TableRow>
+                            )}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Container>
+        </Container>
+    );
 }
 
-                export default Form;
+export default Form;
